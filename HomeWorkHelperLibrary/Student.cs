66186b71ec5c39ce49a87020df0e62009668fd03
Peyton <White>
2019-03-25 10:36:28 -0500

    Created Student Class

--- HomeWorkHelperLibrary/Student.cs
+++ HomeWorkHelperLibrary/Student.cs
@@ -8,6 +8,79 @@ namespace HomeWorkHelperLibrary
 {
     class Student
     {
+        // member variables
+        string _name;
+        string _userName;
+        string _password;
+        string[] _securityQuestions = new string[3];
+        List<Courses> _courseList = new List<Courses>();
+        List<Tasks> _taskList = new List<Tasks>();
+
+        // properties 
+        public string Name {
+
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+        public string UserName {
+
+            get
+            {
+                return _userName;
+
+            }
+           private set
+            {
+                _userName = value;
+            }
+
+        }
+        public string Password
+        {
+
+            get
+            {
+                return _password;
+            }
+            private set
+            {
+                _password = value;
+            }
+        }
+        public string[] SecurityQuestions
+        {
+
+            get
+            {
+                return SecurityQuestions;
+
+            }
+          private set
+            {
+                _securityQuestions = value;
+            }
+
+        }
+
+        //methods 
+        
+        // constructor
+        Student()
+        {
+
+        }
+
+        
+
+
+
+
 
 
     }
