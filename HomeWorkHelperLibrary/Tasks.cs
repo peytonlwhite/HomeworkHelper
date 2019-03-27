7776d743d8bf87c47a59bfa356adbe999876348f
Peyton <White>
2019-03-27 12:49:01 -0500

    fixed the task

--- HomeWorkHelperLibrary/Tasks.cs
+++ HomeWorkHelperLibrary/Tasks.cs
@@ -8,7 +8,53 @@ namespace HomeWorkHelperLibrary
 {
     public class Tasks
     {
+        // member variables 
+        string _taskName;
+        bool _reoccuringTask;
+        DateTime _remindTime;
+        DateTime _dueDate;
+        DateTime _dueDateEnd;
+        //Enum _taskType;
 
+        public string TaskName
+        {
+            get
+            {
+                return _taskName;
+            }
+            private set
+            {
+                _taskName = value;
+            }
+        }
+        public DateTime RemindTime
+        {
+            get
+            {
+                return _remindTime;
+            }
+            private set
+            {
+                _remindTime = value;
+            }
+        }
+
+        public DateTime DueDate
+        {
+            get
+            {
+                return _dueDate;
+            }
+            private set
+            {
+                _dueDate = value;
+            }
+        }
+
+        public Tasks(string taskName)
+        {
+            TaskName = taskName;
+        }
 
     }
 }
