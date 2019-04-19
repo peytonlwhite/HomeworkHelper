fb16973b9a060bf5083a474d8c95e06acba99b10
Jacob Girsky <jgirsky@my.apsu.edu>
2019-04-19 15:27:57 -0500

    Hry

---
+++ HomeworkHelper/DeleteCourse.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HomeWorkHelperLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeworkHelper
+{
+    /// <summary>
+    /// Interaction logic for DeleteCourse.xaml
+    /// </summary>
+    public partial class DeleteCourse : Window
+    {
+        Student student;
+        int courseNums;
+        public DeleteCourse(Student newStudent)
+        {
+            InitializeComponent();
+            student = newStudent;
+            for (int i = 0; i < student.CourseList.Count; i++)
+            {
+                courseNums = (student.CourseList[i].CourseNumber);
+                deleteCB.Items.Add(courseNums);
+            }
+        }
+
+        private void deleteCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
